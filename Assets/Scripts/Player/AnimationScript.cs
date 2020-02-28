b7da48d6b2e875aee5cc8a6ca1b89f6bc227dfdb
Cauehcs <caue.hcs@gmail.com>
2020-02-27 21:00:32 -0300

    Firts Animations Added

---
+++ Assets/Scripts/Player/AnimationScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationScript : MonoBehaviour
+{
+    Animator anim;
+    [SerializeField] bool[] animationStates;
+    //0 - Idle
+    //1 - Idle 2
+
+
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    private void Start()
+    {
+           
+    }
+
+    private void Update()
+    {
+        ToIdle2();
+    }
+    
+    float timeToIdle2;
+    void ToIdle2()
+    {
+        if (animationStates[0]) timeToIdle2 += Time.deltaTime;
+            else timeToIdle2 = 0;
+       
+        if (timeToIdle2 > 20) { animationStates[1] = true; timeToIdle2 = 0; }
+        anim.SetBool("Idle2", animationStates[1]); animationStates[1] = false;
+    }
+}
