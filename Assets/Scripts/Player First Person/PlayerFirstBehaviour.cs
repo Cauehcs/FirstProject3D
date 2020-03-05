37b6ee3a36e19627ccddef392f9c9507d61695b1
Aluno <aluno@naverecife.local>
2020-03-05 14:01:17 -0300

    Interation Canva and Object3D

--- Assets/Scripts/Player First Person/PlayerFirstBehaviour.cs
+++ Assets/Scripts/Player First Person/PlayerFirstBehaviour.cs
@@ -6,16 +6,20 @@ public class PlayerFirstBehaviour : MonoBehaviour
 {
 
     public bool lockMouse = true, jumping;
-    public float sensibility, speed, gravity, jumpForce;
+    public float sensibility, speed, gravity, jumpSpeed;
+    private Vector3 moveDirection = Vector3.zero;
+
 
     private float mouseX = 0.0f, mouseY = 0.0f;
 
     GameObject lookAt;
-    Rigidbody myRG;
+    //Rigidbody myRG; 
+    CharacterController characterController;
 
     private void Awake()
     {
-        myRG = GameObject.Find("Player Body").GetComponent<Rigidbody>();
+        characterController = GameObject.Find("Player Body").GetComponent<CharacterController>();
+        //myRG = GameObject.Find("Player Body").GetComponent<Rigidbody>();
         lookAt = GameObject.Find("LookAt");
     }
 
@@ -37,8 +41,36 @@ public class PlayerFirstBehaviour : MonoBehaviour
         Vector3 verticalMovement = Input.GetAxis("Vertical") * speed * new Vector3(lookAtValue.x, 0f, lookAtValue.z);
         Vector3 horizontalMovement = Input.GetAxis("Horizontal") * transform.right * (speed * 1.5f);
 
-        if (Input.GetKey(KeyCode.LeftShift)) plus = 1.8f; else plus = 1;
-        myRG.velocity = horizontalMovement + ((verticalMovement * -1) / 2.4f) * plus;
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            if (Input.GetAxis("Vertical") == 0 && Input.GetAxis("Horizontal") != 0) ;
+                else plus = 1.8f; 
+        }
+            else plus = 1;
+
+        if (Input.GetAxis("Vertical") == 0 && Input.GetAxis("Horizontal") != 0) { horizontalMovement *= 2.4f; }
+
+        if (characterController.isGrounded)
+        {
+            // We are grounded, so recalculate
+            // move direction directly from axes
+
+            moveDirection = (verticalMovement * -1 + horizontalMovement) * plus;
+            moveDirection *= speed;
+
+            if (Input.GetButton("Jump"))
+            {
+                moveDirection.y = jumpSpeed;
+            }
+        }
+
+        // Apply gravity. Gravity is multiplied by deltaTime twice (once here, and once below
+        // when the moveDirection is multiplied by deltaTime). This is because gravity should be applied
+        // as an acceleration (ms^-2)
+        moveDirection.y -= gravity * Time.deltaTime;
+
+        // Move the controller
+        characterController.Move(moveDirection * Time.deltaTime);
     }
 
     void MouseMovement()
